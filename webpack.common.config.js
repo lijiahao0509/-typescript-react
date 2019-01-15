92fc5c8d0101ba5a5e9d0ed2784757581d348cc5
李嘉浩 <1040348501@qq.com>
2019-01-15 14:51:01 +0800

    初始提交

---
+++ webpack.common.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+        publicPath:'/dist/' 
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            { test: /\.css$/, loader: "style-loader!css-loader?modules" },
+            { test: /\.(png|jpe?g|gif)/, loader: "file-loader" }
+        ]
+    },
+    plugins: [
+    ],
+};
